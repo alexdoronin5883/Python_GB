3d966dfb362514f5071690fff0e639b05cf99c62
Александр Доронин <107746307+alexdoronin5883@users.noreply.github.com>
2023-07-07 16:32:44 +0300

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -3,15 +3,15 @@
 # строковое представление. Функцию hex
 # используйте для проверки своего результата
 
-# def to_hex(num):
-#     hex_digits = "0123456789abcdef"
-#     hex_str = ""
-#     while num > 0:
-#         hex_str = hex_digits[num % 16] + hex_str
-#         num = num // 16
-#     return hex_str
-#
-# # Пример использования функции
-# num = int(input('Введите число: '))
-# hex_str = to_hex(num)
-# print(f"Шестнадцатеричное представление числа {num} - {hex_str}")
+ def to_hex(num):
+    hex_digits = "0123456789abcdef"
+    hex_str = ""
+    while num > 0:
+        hex_str = hex_digits[num % 16] + hex_str
+        num = num // 16
+    return hex_str
+
+# Пример использования функции
+num = int(input('Введите число: '))
+hex_str = to_hex(num)
+print(f"Шестнадцатеричное представление числа {num} - {hex_str}")
